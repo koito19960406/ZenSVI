01875d7d40f472298d8043f21cf73043dea40271
koito19960406 <61777450+koito19960406@users.noreply.github.com>
2024-02-19 16:14:49 +0800

    added single file input

--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -8,7 +8,7 @@ class TestSegmentation(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         pass
-    
+        
     def test_mapillary_panoptic(self):
         segmenter = Segmenter(dataset="mapillary", task="panoptic")
         image_input = "tests/data/input/images"
@@ -43,6 +43,25 @@ class TestSegmentation(unittest.TestCase):
         image_output = "tests/data/output/segmentation/large_image"
         summary_output = "tests/data/output/segmentation/large_image_summary"
         segmenter.segment(image_input, dir_image_output = image_output, dir_segmentation_summary_output = summary_output, csv_format = "wide", max_workers = 1)
+    
+    def test_single_image(self):
+        segmenter = Segmenter(dataset="mapillary", task="panoptic")
+        image_input = "tests/data/input/images/-3vfS0_iiYVZKh_LEVlHew.jpg"
+        image_output = "tests/data/output/segmentation/single_image"
+        summary_output = "tests/data/output/segmentation/single_image_summary"
+        segmenter.segment(image_input, dir_image_output = image_output, dir_segmentation_summary_output = summary_output, csv_format = "wide", max_workers = 1)
+    
+    def test_calculate_pixel_ratio_post_process(self):
+        segmenter = Segmenter(dataset="mapillary", task="panoptic")
+        image_input = "tests/data/input/cityscapes_semantic"
+        image_output = "tests/data/output/segmentation/calculate_pixel_ratio_post_process"
+        segmenter.calculate_pixel_ratio_post_process(image_input, image_output)
+    
+    def test_calculate_pixel_ratio_post_process_single_file(self):
+        segmenter = Segmenter(dataset="mapillary", task="panoptic")
+        image_input = "tests/data/input/cityscapes_semantic/-3vfS0_iiYVZKh_LEVlHew_colored_segmented.png"
+        image_output = "tests/data/output/segmentation/calculate_pixel_ratio_post_process_single_file"
+        segmenter.calculate_pixel_ratio_post_process(image_input, image_output)
         
 if __name__ == "__main__":
     unittest.main()
