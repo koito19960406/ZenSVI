6420399a33c3dfc0e183618e8bb8d143b2b3413e
Xiucheng <xcleung@outlook.com>
2024-05-15 13:34:25 +0800

    add files for place pulse classification

---
+++ src/zensvi/cv/classification/perception.py
@@ -0,0 +1,158 @@
+from pathlib import Path
+from typing import List, Tuple, Union
+from torch.utils.data import Dataset, DataLoader
+from huggingface_hub import hf_hub_download
+from torchvision import transforms
+from PIL import Image
+import torch
+import pandas as pd
+import tqdm
+
+from .utils.place_pulse import PlacePulseClassificationModel
+from .base import BaseClassifier
+
+
+class ImageDataset(Dataset):
+    def __init__(self, image_files: List[Path]):
+        self.image_files = [
+            image_file
+            for image_file in image_files
+            if image_file.suffix.lower() in [".jpg", ".jpeg", ".png"]
+            and not image_file.name.startswith(".")
+        ]
+
+        # Image transformations
+        self.transform = transforms.Compose(
+            [
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ), 
+            ]
+        )
+
+    def __len__(self) -> int:
+        return len(self.image_files)
+
+    def __getitem__(self, idx: int) -> Tuple[str, torch.Tensor]:
+        image_file = self.image_files[idx]
+        img = Image.open(image_file).convert('RGB') 
+        img = self.transform(img) 
+
+        return str(image_file), img
+
+    def collate_fn(self, data: List[Tuple[str, torch.Tensor]]) -> Tuple[List[str], torch.Tensor]:
+        """
+        Custom collate function for the dataset.
+
+        Args:
+            data (List[Tuple[str, torch.Tensor]]): List of tuples containing image file path and transformed image tensor.
+
+        Returns:
+            Tuple[List[str], torch.Tensor]: Tuple containing lists of image file paths and a batch of image tensors.
+        """
+        image_files, images = zip(*data)
+        images = torch.stack(images)  # Stack images to create a batch
+        return list(image_files), images
+
+
+class ClassifierPerception(BaseClassifier):
+    """
+    A classifier for evaluating the perception of streetscape based on a given study.
+
+    :param device: The device that the model should be loaded onto. Options are "cpu", "cuda", or "mps".
+        If `None`, the model tries to use a GPU if available; otherwise, falls back to CPU.
+    :param perception_study: The specific perception study for which the model is trained, including "safer", "livelier", "wealthier", "more beautiful", "more boring", "more depressing". This affects the checkpoint file used.
+    :type device: str, optional
+    :type perception_study: str
+    """
+
+    def __init__(self, perception_study, device=None):
+        super().__init__(device)
+        self.device = self._get_device(device)
+        self.perception_study = perception_study
+
+        file_name = f"{perception_study}.pth"
+        checkpoint_path = hf_hub_download(
+            repo_id="seshing/placepulse", filename=file_name, local_dir=Path(__file__).parent.parent.parent.parent.parent / "models"
+        )
+
+        # Now load the model
+        self.model = PlacePulseClassificationModel(num_classes=5)
+        self.model = self.load_checkpoint(self.model, checkpoint_path)
+        self.model.eval()
+        self.model.to(self.device)
+        
+    def _save_results_to_file(
+        self, results, dir_output, file_name, save_format="csv json"
+    ):
+        df = pd.DataFrame(results)
+        dir_output = Path(dir_output)
+        dir_output.mkdir(parents=True, exist_ok=True)
+        if "csv" in save_format:
+            file_path = dir_output / f"{file_name}.csv"
+            df.to_csv(file_path, index=False)
+        if "json" in save_format:
+            file_path = dir_output / f"{file_name}.json"
+            df.to_json(file_path, orient="records")
+
+    def load_checkpoint(self, model, checkpoint_path):
+        checkpoint = torch.load(checkpoint_path, map_location=self.device)
+        model.load_state_dict(checkpoint)
+        return model
+
+    def classify(
+        self,
+        dir_input: Union[str, Path],
+        dir_summary_output: Union[str, Path],
+        batch_size=1,
+        save_format="json csv",
+    ) -> List[str]:
+        """
+        Classifies images based on human perception of streetscapes from the specified perception study. The output file can be saved in JSON and/or CSV format and will contain the final perception score for each image.
+
+        :param dir_input: Directory containing input images.
+        :type dir_input: Union[str, Path]
+        :param dir_summary_output: Directory to save summary output. If None, output is not saved.
+        :type dir_summary_output: Union[str, Path, None]
+        :param batch_size: Batch size for inference, defaults to 1.
+        :type batch_size: int, optional
+        :param save_format: Save format for the output, defaults to "json csv". Options are "json" and "csv". Please add a space between options.
+        :type save_format: str, optional
+        """
+        # Prepare output directories
+        if dir_summary_output:
+            Path(dir_summary_output).mkdir(parents=True, exist_ok=True)
+
+        # get all the images in dir_input
+        if Path(dir_input).is_file():
+            img_paths = [Path(dir_input)]
+        else:
+            img_paths = list(Path(dir_input).rglob("*.[jJp][pPn][gG]"))
+
+        dataset = ImageDataset(img_paths)
+        dataloader = DataLoader(
+            dataset, batch_size=batch_size, shuffle=False
+        )
+
+        results = []
+        with torch.no_grad():
+            for image_files, images in tqdm.tqdm(dataloader, desc=f"Evaluating human perception of study: {self.perception_study}"):
+                images = images.to(self.device, dtype=torch.float32)
+
+                custom_scores = self.model(images)
+                for image_file, score in zip(image_files, custom_scores):
+                    results.append({
+                        "filename_key": str(Path(image_file).stem),
+                        f"{self.perception_study}": score.item()
+                    })
+
+        # Save the results to JSON and/or CSV
+        self._save_results_to_file(
+            results,
+            dir_summary_output,
+            "results",
+            save_format=save_format,
+        )
+        return results
---
+++ src/zensvi/cv/classification/utils/place_pulse.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from torchvision.models.resnet import ResNet50_Weights
+from torch.nn import functional as F
+# import pytorch_lightning as pl
+
+class PlacePulseClassificationModel(nn.Module):
+    def __init__(self, lr=0.0001, num_classes=None, **kwargs):
+        super().__init__()
+        self.resnet50 = models.resnet50(weights=None)
+        self.lr = lr
+
+        num_ftrs = self.resnet50.fc.in_features
+        self.resnet50.fc = nn.Identity()
+
+        # Adding custom fully connected layers
+        self.fc1 = nn.Linear(num_ftrs, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 128)
+        self.fc4 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = self.resnet50(x)  # Features from ResNet50
+
+        # Passing through the custom fully connected layers
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+
+        # Convert logits to probabilities using softmax
+        probabilities = F.softmax(x, dim=1)
+        scores = self.calculate_score(probabilities)
+        return scores
+
+    def calculate_score(self, probabilities):
+        # Define weights for each class
+        weights = torch.tensor([1, 3, 5, 7, 9], device=probabilities.device)
+        # weights = torch.tensor([5/6, 15/6, 25/6, 35/6, 45/6, 55/6], device=probabilities.device)
+
+        # Ensure the weights are compatible with the probabilities
+        if probabilities.shape[1] != len(weights):
+            raise ValueError("The number of classes does not match the number of weights.")
+
+        # Compute the weighted score for each image
+        scores = torch.sum(probabilities * weights, dim=1)
+        return scores
+    
+    # def evaluate(self, batch):
+    #     x, y = batch
+    #     scores = self(x)  # Directly obtain scores from the model's forward pass
+    #     return {'scores': scores}
