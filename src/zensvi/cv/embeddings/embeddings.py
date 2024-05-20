046d4442f9ec3f1602d6d8f05135fe2fb795d0e5
Mahmoud <mahmoudouf@Mahmouds-MacBook-Pro-2.local>
2024-05-20 22:00:07 +0800

    added cosine similarity

--- src/zensvi/cv/embeddings/embeddings.py
+++ src/zensvi/cv/embeddings/embeddings.py
@@ -1,20 +1,21 @@
-# Description: This file contains the code to extract embeddings from a given image using a pre-trained model.
-# Author : Mahmoud A. (arch.mahmoud.ouf111@gmail.com)
-# This module is based on img2vec_pytorch
-
-import torch.nn as nn
-import torchvision.models as models
-import torchvision.transforms as transforms
-from torch.autograd import Variable
-from typing import List
-from PIL import Image
-from img2vec_pytorch import Img2Vec
-import numpy as np
-from collections import namedtuple
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+import os
 import tqdm
+import torch
+import numpy as np
 import pandas as pd
+from PIL import Image
+import torch.nn as nn
+from shutil import copyfile
+from typing import List, Union
+import matplotlib.pyplot as plt
+from collections import namedtuple
+from sklearn.cluster import KMeans
+import torchvision.models as models
+from torch.autograd import Variable
+from img2vec_pytorch import Img2Vec
+from sklearn.decomposition import PCA
+import torchvision.transforms as transforms
+
 
 _Model = namedtuple('Model', ['name', 'layer', 'layer_output_size'])
 
@@ -65,7 +66,8 @@ class EmbeddingVector:
 class Embeddings:
     def __init__(self,
                  model_name: str ='resnet-18',
-                 cuda: bool =False
+                 cuda: bool =False,
+                 tensor: bool = True
                  ):
         """
         :param model_name: name of the model to be used for extracting embeddings (default: 'resnet-18') 
@@ -79,6 +81,7 @@ class Embeddings:
         self.model, self.extraction_layer = self.get_model_and_layer()
         self.model.eval()
         self.cuda = cuda
+        self.tensor = tensor
 
     def load_image(self, image_path):
         """
@@ -87,9 +90,6 @@ class Embeddings:
         """
         img = Image.open(image_path)
         img = img.resize((224, 224))
-        img = transforms.ToTensor()(img)
-        img = img.unsqueeze(0)
-        print(img.shape)
         return img
 
     def get_model_and_layer(self):
@@ -101,18 +101,24 @@ class Embeddings:
         return model, layer
     
 
-    def get_image_embedding(self, image_path):
+    def get_image_embedding(self, 
+                            image_path: Union[List[str], str], 
+                            tensor: bool = None, 
+                            cuda: bool = None):
         """
         :param image_path: path to the image
         :return: image embedding
         """
+        if not tensor:
+            tensor = self.tensor
+        if not cuda:
+            cuda = self.cuda
+            
+        img2vec = Img2Vec(cuda=cuda)
+
         img = self.load_image(image_path)
-        if self.cuda:
-            img = Variable(img).cuda()
-        else:
-            img = Variable(img)
-        result = self.model(img)
-        return result.detach().numpy()
+        return img2vec.get_vec(img)
+        
     
 
     def generate_embedding(self, 
@@ -138,59 +144,65 @@ class Embeddings:
         :param emb2: embedding 2
         :return: cosine similarity between the two embeddings
         """
+        # make sure that emb1 and emb2 are tensors of the same shape:
+        emb1 = torch.tensor(emb1)
+        emb2 = torch.tensor(emb2)
+
         cos = nn.CosineSimilarity(dim=1, eps=1e-6)
-        cos_sim = cos(emb1.unsqueeze(0),
-                    emb2.unsqueeze(0))
+        cos_sim = cos(emb1.reshape(1, -1),
+              emb2.reshape(1, -1))[0]
+
         print('\nCosine similarity: {0}\n'.format(cos_sim))
         return cos_sim
 
     def cluster(self, 
-                dir_embeddings_output: List[str],
-                dir_summary_output: str,
-                num_clusters: int =100,
-                batch_size: int =100):
+                input_path,
+                vec_length: int = 512,
+                k_value: int = 2,
+                ):
         """
         :param dir_embeddings_output: directory containing the embeddings
         :param dir_summary_output: directory to save the summary of the clustering
         :param batch_size: batch size for clustering (default: 100)
         """
-        embeddings = []
-        for i in tqdm.tqdm(range(len(dir_embeddings_output))):
-            embeddings.append(np.load(dir_embeddings_output + f'embeddings_{i}.npy'))
-        embeddings = np.concatenate(embeddings, axis=0)
-        kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(embeddings)
-        np.save(dir_summary_output + 'labels.npy', kmeans.labels_)
-        np.save(dir_summary_output + 'centers.npy', kmeans.cluster_centers_)
-        print('Clustering done!')
+        files = os.listdir(input_path)
+        img2vec = Img2Vec(self.model, cuda=self.cuda)
+        samples = len(files)
+        vec_mat = np.zeros((samples, vec_length))
+        sample_indices = np.random.choice(range(0, len(files)), size=samples, replace=False)
+
+        print('Reading images...')
+        for index, i in enumerate(sample_indices):
+            file = files[i]
+            filename = os.fsdecode(file)
+            img = Image.open(os.path.join(input_path, filename)).convert('RGB')
+            vec = img2vec.get_vec(img)
+            vec_mat[index, :] = vec
+
+        print('Applying PCA...')
+        reduced_data = PCA(n_components=2).fit_transform(vec_mat)
+        kmeans = KMeans(init='k-means++', n_clusters=k_value, n_init=10)
+        kmeans.fit(reduced_data)
+
+        # Create a folder for each cluster (0, 1, 2, ..)
+        for i in set(kmeans.labels_):
+            try:
+                os.mkdir('./' + str(i))
+            except FileExistsError:
+                continue
+
+        print('Predicting...')
+        preds = kmeans.predict(reduced_data)
+
+        print('Copying images...')
+        for index, i in enumerate(sample_indices):
+            file = files[i]
+            filename = os.fsdecode(file)
+            copyfile(input_path + '/' + filename, './' + str(preds[index]) + '/' + filename)
+
+        print('Done!')
 
 
-    def search_similar(self, 
-                        image_path : str,
-                        dir_images: List[str], 
-                        dir_embeddings_output : str, 
-                        dir_summary_output: str, 
-                        num_similar: int =5
-                        ):
-        """
-        :param image_path: path to the image to search for similar images
-        :param dir_images: directory containing the images
-        :param dir_embeddings_output: directory containing the embeddings
-        :param dir_summary_output: directory containing the summary of the clustering
-        :param num_similar: number of similar images to return (default: 5)
-        :return: similar images
-        """
-        # img = self.load_image(image_path)
-        # img2vec = Img2Vec(cuda=self.cuda)
-        # fvector = img2vec.get_vec([img])
-        # labels = np.load(dir_summary_output + 'labels.npy')
-        # centers = np.load(dir_summary_output + 'centers.npy')
-        # kmeans = KMeans(n_clusters=centers.shape[0], init=centers, n_init=1)
-        # kmeans.fit(fvector)
-        # similar_images = []
-        # for i in range(len(labels)):
-        #     if labels[i] == kmeans.labels_[0]:
-        #         similar_images.append(dir_images[i])
-        # return similar_images[:num_similar]
 
 if __name__ == '__main__':
     emb = Embeddings()
