4046ffa58952a66452d39b4e4779e271b7376b74
koito19960406 <koichi19960406@gmail.com>
2024-07-03 19:39:12 +0800

    added a new argument for using only upper half

--- tests/test_image_transformation.py
+++ tests/test_image_transformation.py
@@ -56,6 +56,26 @@ class TestImageTransformer(unittest.TestCase):
                 ]
             ]
         )
+    
+    def test_upper_half(self):
+        dir_input = "tests/data/input/images/-3vfS0_iiYVZKh_LEVlHew.jpg"
+        dir_output = str(self.output / "upper_half")
+        transformer = ImageTransformer(dir_input, dir_output, log_path=self.output / "transformation.log")
+        transformer.transform_images(use_upper_half=True)
+        # assert True if all the subdirectories are created in the output directory and files are not empty
+        assert all(
+            [
+                Path(dir_output, sub_dir).is_dir()
+                and len(list(Path(dir_output, sub_dir).rglob("*"))) > 0
+                for sub_dir in [
+                    "equidistant_fisheye",
+                    "equisolid_fisheye",
+                    "orthographic_fisheye",
+                    "perspective",
+                    "stereographic_fisheye",
+                ]
+            ]
+        )
 
 if __name__ == "__main__":
     unittest.main()
